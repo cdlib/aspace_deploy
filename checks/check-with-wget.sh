8bc3a356dd9d7a60dedfe6beb6ee8e9dc1ddf861
Mark Redar <mredar@gmail.com>
2015-11-25 11:31:32 -0800

    Add -s silent option for using in automated fashion.

--- checks/check-with-wget.sh
+++ checks/check-with-wget.sh
@@ -1,15 +1,35 @@
 
+silent=0
+
+function usage () {
+	echo "check-with-wget.sh (-s for silent operation) (-h for this msg)"
+}
+
+while getopts hs FLAG; do
+	case $FLAG in
+		s)
+			silent=1
+			;;
+		h)
+			usage
+			exit 0
+			;;
+	esac
+done
 
 declare -a clients=("ucbeda" "uclaclark" "ucm" "ucmppdc" "ucsc" "ucsf" "ucsfmc"
-"ucrcmp" 
-"this_is_a_failure" )
+"ucrcmp" )
 
 function check_public_url () {
-    echo "URL: http://public.${client}.aspace.cdlib.org"
+	if [ $silent -ne 1 ] ; then
+		echo "URL: http://public.${client}.aspace.cdlib.org"
+	fi
     wget -q http://public.${client}.aspace.cdlib.org > /dev/null
     last_exit=$?
     if [ $last_exit -ne 0 ] ; then
-        echo -e "\033[31mFor ${client} wget exit is $last_exit\033[0m"
+		if [ $silent -ne 1 ] ; then
+			echo -e "\033[31mFor ${client} wget exit is $last_exit\033[0m"
+		fi
         PUBLIC_ERRS+="${client} "
     else
         rm index.html
@@ -17,11 +37,15 @@ function check_public_url () {
 }
 
 function check_private_url () {
-    echo "URL: https://${client}.aspace.cdlib.org"
+	if [ $silent -ne 1 ] ; then
+		echo "URL: https://${client}.aspace.cdlib.org"
+	fi
     wget -q --no-check-certificate https://${client}.aspace.cdlib.org > /dev/null
     last_exit=$?
     if [ $last_exit -ne 0 ] ; then
-        echo -e "\033[31mFor ${client} wget exit is $last_exit\033[0m"
+		if [ $silent -ne 1 ] ; then
+			echo -e "\033[31mFor ${client} wget exit is $last_exit\033[0m"
+	    fi
         PRIVATE_ERRS+="${client} "
     else
         rm index.html
@@ -35,9 +59,17 @@ do
 	check_public_url client
 	check_private_url client
 done
-if [ ${#PUBLIC_ERRS[@]} -ne 0 ]; then
-    echo -e "\033[31mPUBLIC ERRORS: $PUBLIC_ERRS\033[0m"
+if [ $silent -ne 1 ] ; then
+	if [ ${#PUBLIC_ERRS[@]} -ne 0 ]; then
+		echo -e "\033[31mPUBLIC ERRORS: $PUBLIC_ERRS\033[0m"
+	fi
+	if [ ${#PRIVATE_ERRS[@]} -ne 0 ]; then
+		echo -e "\033[31mPRIVATE ERRORS: $PRIVATE_ERRS\033[0m"
+	fi
 fi
 if [ ${#PRIVATE_ERRS[@]} -ne 0 ]; then
-    echo -e "\033[31mPRIVATE ERRORS: $PRIVATE_ERRS\033[0m"
+	exit 11
+fi
+if [ ${#PUBLIC_ERRS[@]} -ne 0 ]; then
+	exit 12
 fi
