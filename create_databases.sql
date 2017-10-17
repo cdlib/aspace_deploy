915155a1ad3e0acbe8f98fa25e803e0c2b714e1f
Mark Redar <mredar@gmail.com>
2017-10-17 18:13:53 +0000

    Add create_databases.sql script

---
+++ create_databases.sql
@@ -0,0 +1,10 @@
+create database ucsf default character set utf8 default collate utf8_general_ci;
+create database ucmppdc default character set utf8 default collate utf8_general_ci;
+create database ucbeda default character set utf8 default collate utf8_general_ci;
+create database ucsc default character set utf8 default collate utf8_general_ci;
+create database ucrcmp default character set utf8 default collate utf8_general_ci;
+create database uclaclark default character set utf8 default collate utf8_general_ci;
+create database ucm default character set utf8 default collate utf8_general_ci;
+create database ucla default character set utf8 default collate utf8_general_ci;
+create database uclacsrc default character set utf8 default collate utf8_general_ci;
+create database uci default character set utf8 default collate utf8_general_ci;
